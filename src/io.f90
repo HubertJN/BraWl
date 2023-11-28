d35380aea57f26b46fbc8c022573188bd1aea3a4
Chris Woodgate <c.woodgate@warwick.ac.uk>
2023-11-28 17:05:24 +0000

    Work on io routines

--- src/io.f90
+++ src/io.f90
@@ -9,6 +9,8 @@ module io
 
   use kinds
   use mpi_shared_data
+  use command_line
+  use display
   
   implicit none
 
@@ -46,21 +48,57 @@ module io
   end subroutine write_info
 
   !--------------------------------------------------------------------!
+  ! Subroutine to make directories for storing data                    !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
+  subroutine make_data_directories(my_rank)
+    integer :: my_rank
+
+    ! make a directory for the grid states, diagnostics, 
+    ! and radial_densities for each thread
+    if(my_rank == 0) call execute_command_line('mkdir -p grids')
+    if(my_rank == 0) call execute_command_line('mkdir -p diagnostics')
+    if(my_rank == 0) call execute_command_line('mkdir -p radial_densities')
+  end subroutine make_data_directories
+
+  !--------------------------------------------------------------------!
   ! Subroutine to parse control file                                   !
   !                                                                    !
   ! C. D. Woodgate,  Warwick                                      2023 !
   !--------------------------------------------------------------------!
-  subroutine read_control_file(filename, parameters)
+  subroutine read_control_file(filename, parameters, my_rank)
 
     character(len=*), intent(in) :: filename
-    logical, dimension(8) :: check
+    logical, dimension(11) :: check
     type(run_params) :: parameters
     character(len=100) :: buffer, label
-    integer :: line, pos, ios, ierr
+    integer :: line, pos, ios, ierr, my_rank
+    logical :: exists
 
     check = .false.
 
     ios=0; line=0
+
+    ! Defaults if these are not specified.
+    parameters%wc_range = 2
+    parameters%delta_T = 1
+    parameters%T_steps = 1
+    parameters%lro = .false.
+    parameters%nbr_swap = .false.
+    parameters%sample_steps = 1000
+    parameters%burn_in = .false.
+    parameters%burn_in_steps = 1000
+
+    inquire(file=trim(filename), exist=exists)
+
+    if (.not. exists) then
+      stop 'Could not find input file ' // trim(filename)
+    end if
+
+    if(my_rank == 0) then
+      write(6,'(26("-"),x,"Parsing input file",x,26("-"),/)')
+    end if
 
     open(15, file=filename, iostat=ios)
 
@@ -76,59 +114,47 @@ module io
         buffer = buffer(pos+1:)
 
         select case (label)
+        case ('mode')  
+          read(buffer, *, iostat=ios) parameters%mode
+          check(1) = .true.
         case ('n_1')  
           read(buffer, *, iostat=ios) parameters%n_1
-          check(1) = .true.
+          check(2) = .true.
         case ('n_2')  
           read(buffer, *, iostat=ios) parameters%n_2
-          check(2) = .true.
+          check(3) = .true.
         case ('n_3')  
           read(buffer, *, iostat=ios) parameters%n_3
-          check(3) = .true.
-        case ('n_basis')  
-          read(buffer, *, iostat=ios) parameters%n_basis
-          check(3) = .true.
-        case ('a')  
-          read(buffer, *, iostat=ios) parameters%a
-        case ('c_a')  
-          read(buffer, *, iostat=ios) parameters%c_a
-        case ('b')  
-          read(buffer, *, iostat=ios) parameters%b
-        case ('c_b')  
-          read(buffer, *, iostat=ios) parameters%c_b
-        case ('c')  
-          read(buffer, *, iostat=ios) parameters%c
-        case ('c_c')  
-          read(buffer, *, iostat=ios) parameters%c_c
-        case ('d')  
-          read(buffer, *, iostat=ios) parameters%d
-        case ('c_d')  
-          read(buffer, *, iostat=ios) parameters%c_d
-        case ('e')  
-          read(buffer, *, iostat=ios) parameters%e
-        case ('c_e')  
-          read(buffer, *, iostat=ios) parameters%c_e
-        case ('n_steps')  
-          read(buffer, *, iostat=ios) parameters%mc_steps
           check(4) = .true.
+        case ('burn_in')  
+          read(buffer, *, iostat=ios) parameters%burn_in
+        case ('burn_in_steps')  
+          read(buffer, *, iostat=ios) parameters%burn_in_steps
+        case ('n_mc_steps')  
+          read(buffer, *, iostat=ios) parameters%mc_steps
+          check(5) = .true.
+        case ('sample_steps')  
+          read(buffer, *, iostat=ios) parameters%sample_steps
         case ('n_species')  
           read(buffer, *, iostat=ios) parameters%n_species
-          check(5) = .true.
-        case ('lattice_parameter')  
-          read(buffer, *, iostat=ios) parameters%lattice_parameter
+          check(6) = .true.
         case ('lattice')  
           read(buffer, *, iostat=ios) parameters%lattice
-          check(6) = .true.
-        case ('exchange')  
-          read(buffer, *, iostat=ios) parameters%exchange
           check(7) = .true.
-        case ('n_shells')  
-          read(buffer, *, iostat=ios) parameters%n_shells
+        case ('lattice_parameter')  
+          read(buffer, *, iostat=ios) parameters%lattice_parameter
           check(8) = .true.
-        case ('radial_d')  
-          read(buffer, *, iostat=ios) parameters%radial_d
-        case ('T_start')  
-          read(buffer, *, iostat=ios) parameters%T_start
+        case ('interaction_file')  
+          read(buffer, *, iostat=ios) parameters%interaction_file
+          check(9) = .true.
+        case ('interaction_range')  
+          read(buffer, *, iostat=ios) parameters%interaction_range
+          check(10) = .true.
+        case ('wc_range')  
+          read(buffer, *, iostat=ios) parameters%wc_range
+        case ('T')  
+          read(buffer, *, iostat=ios) parameters%T
+          check(11) = .true.
         case ('delta_T')  
           read(buffer, *, iostat=ios) parameters%delta_T
         case ('T_steps')  
@@ -142,6 +168,48 @@ module io
       end if
     end do
 
+
+    allocate(parameters%species_names(parameters%n_species))
+    allocate(parameters%species_concentrations(0:parameters%n_species))
+
+    parameters%species_concentrations = 0.0_real64
+
+    line=0
+
+    close(15)
+
+    open(15, file=filename, iostat=ios)
+
+
+    do while (ios==0)
+
+      read(15, "(A)", iostat=ios) buffer
+
+      if(ios==0) then
+        line=line+1
+
+        pos = scan(buffer, '=')
+        label=buffer(1:pos-1)
+        buffer = buffer(pos+1:)
+
+        select case (label)
+        case ('species_names')  
+          read(buffer, *, iostat=ios) parameters%species_names
+          check(1) = .true.
+        case ('species_concentrations')  
+          read(buffer, *, iostat=ios) parameters%species_concentrations(1:)
+          check(2) = .true.
+        case default
+        end select
+      end if
+    end do
+
+    close(15)
+
+    if (.not. any(check)) then
+      stop 'Missing parameter in input file'
+    end if
+
   end subroutine read_control_file
 
   subroutine print_parse()
@@ -153,52 +221,94 @@ module io
 
   subroutine echo_control_file(parameters)
     type(run_params) :: parameters
+    integer :: i
 
-    print*, '# Read n_1 = ', parameters%n_1
-    print*, '# Read n_2 = ', parameters%n_2
-    print*, '# Read n_3 = ', parameters%n_3
-    print*, '# Read n_basis = ', parameters%n_basis
-    print*, '# Read a = ', parameters%a
-    print*, '# Read c_a = ', parameters%c_a
-    print*, '# Read b = ', parameters%b
-    print*, '# Read c_b = ', parameters%c_b
-    if (parameters%n_species .gt. 2) then
-      print*, '# Read c = ', parameters%c
-      print*, '# Read c_c = ', parameters%c_c
-    end if
-    if (parameters%n_species .gt. 3) then
-      print*, '# Read d = ', parameters%d
-      print*, '# Read c_d = ', parameters%c_d
-    end if
-    if (parameters%n_species .gt. 4) then
-      print*, '# Read e = ', parameters%e
-      print*, '# Read c_e = ', parameters%c_e
-    end if
-    print*, '# Read n_steps = ', parameters%mc_steps
-    print*, '# Read n_species = ', parameters%n_species
-    print*, '# Read lattice = ', parameters%lattice_parameter
-    print*, '# Read lattice = ', parameters%lattice
-    print*, '# Read exchange = ', parameters%exchange
-    print*, '# Read n_shells = ', parameters%n_shells
-    print*, '# Read radial_d = ', parameters%radial_d
-    print*, '# Read T_start = ', parameters%T_start
-    print*, '# Read delta_T = ', parameters%delta_T
-    print*, '# Read T_steps = ', parameters%T_steps
-    print*, '# Read nbr_swap = ', parameters%lro
-    print*, '# Read nbr_swap = ', parameters%nbr_swap
-    print*, ' ', parameters%nbr_swap
+    print*, ' Read n_1 = ', parameters%n_1
+    print*, ' Read n_2 = ', parameters%n_2
+    print*, ' Read n_3 = ', parameters%n_3
+    print*, ' Read n_basis = ', parameters%n_basis
+    print*, ' Read n_steps = ', parameters%mc_steps
+    print*, ' Read n_species = ', parameters%n_species
+    print*, ' Read lattice parameter = ', parameters%lattice_parameter
+    print*, ' Read lattice = ', parameters%lattice
+    print*, ' Read interaction_file = ', parameters%interaction_file
+    print*, ' Read wc_range = ', parameters%wc_range
+    print*, ' Read T = ', parameters%T
+    print*, ' Read delta_T = ', parameters%delta_T
+    print*, ' Read T_steps = ', parameters%T_steps
+    print*, ' Read lro = ', parameters%lro
+    print*, ' Read nbr_swap = ', parameters%nbr_swap
+
+    do i=1, parameters%n_species
+      print*, ' Read species ', i, ' = ', parameters%species_names(i), ' at concentration ', parameters%species_concentrations(i)
+    enddo
 
   end subroutine echo_control_file
 
   !-----------------------------------------------------!
   ! Subroutine to read in exchange parameters from file !
   !-----------------------------------------------------!
-  subroutine read_exchange(setup)
+  subroutine read_exchange(setup, my_rank)
     type(run_params) , intent(in) :: setup
+    integer :: my_rank
+
+    if(my_rank == 0) then
+      write(6,'(15("-"),x,"Reading atom-atom interaction parameters",x,15("-"),/)')
+    end if
+
     V_ex = 0.0_real64
-    open(16, file=setup%exchange)
+    open(16, file=setup%interaction_file)
     read(16,*) V_ex   
     close(16)
+
+    if (my_rank .eq. 0) then
+      ! Print it out as a sanity check for user
+      call pretty_print_exchange(setup)
+    end if
+
+    if(my_rank == 0) then
+      write(6,'(72("-"),/)')
+    end if
+
   end subroutine read_exchange
+
+  !-----------------------------------------------------!
+  ! Subroutine to read in exchange parameters from file !
+  !-----------------------------------------------------!
+  subroutine parse_inputs(setup, my_rank)
+    type(run_params) :: setup
+    integer :: my_rank
+    character(len=30) :: control
+
+    ! Parse the command line arguments
+    call parse_args()
+
+    ! Parse the name of the input file
+    if(my_rank == 0) then
+      write(6,'(22("-"),x,"Parsing name of input file",x,22("-"),/)')
+    end if
+      if(.not. get_arg('control', control)) then
+        if (my_rank == 0) then
+          print*, 'Input file not specified with "control=<name>"'
+          print*, 'Defaulting to searching for "input.txt"'
+          print*, ' '
+        end if
+        control = 'input.txt'
+      else
+        if (my_rank == 0) then
+          print*, 'Input file name is: ', control
+          print*, ' '
+        end if
+      end if
+
+    ! Read the input file
+    call read_control_file(control, setup, my_rank)
+
+    if(my_rank == 0) then
+      call echo_control_file(setup)
+      write(6,'(/,20("-"),x,"Parsed input file successfully",x,20("-"),/)')
+    end if
+
+  end subroutine parse_inputs
 
 end module io
