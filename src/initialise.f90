7d330cc8666c9e927e112bb2e12acce751c11f14
Chris Woodgate <c.woodgate@warwick.ac.uk>
2023-12-01 11:02:57 +0000

    FCC initialisation and comment fixes.

--- src/initialise.f90
+++ src/initialise.f90
@@ -1,10 +1,10 @@
-!----------------------------------------------------!
-! File with module containing functions and routines !
-! to call                                            !
-!                                                    !
-! C Woodgate, Warwick                           2020 !
-!----------------------------------------------------!
-
+!----------------------------------------------------------------------!
+! initialise.f90                                                       !
+!                                                                      !
+! Module with routines associated with initialising a simulation.      !
+!                                                                      !
+! C. D. Woodgate,  Warwick                                        2023 !
+!----------------------------------------------------------------------!
 module initialise
 
   use kinds
@@ -13,15 +13,40 @@ module initialise
   use c_functions
   use energetics
   use random_site
-  use metropolis
+  use comms
   
   implicit none
 
   contains
 
-  !----------------------------------------------------!
-  ! Subroutine to allocate memory for rank 1 processor !
-  !----------------------------------------------------!
+  !--------------------------------------------------------------------!
+  ! Subroutine to initialise the PNRG                                  !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
+  subroutine initialise_pnrg(seedtime)
+    integer :: seedtime
+
+    if(my_rank == 0) then
+      write(6,'(17("-"),x,"Initialising random number generators",x,16("-"),/)')
+    end if
+
+    ! Initialise the prng
+    seed = f90_init_genrand(seedtime, int(my_rank, kind=C_INT))
+
+    call comms_wait()
+    print*, 'Thread ', my_rank, ' has seed ', seed
+    call flush(6)
+
+    call comms_wait()
+
+  end subroutine initialise_pnrg
+
+  !--------------------------------------------------------------------!
+  ! Subroutine to initialise arrays for storing averages (rank 1 only) !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
   subroutine initialise_global_arrays(setup)
     type(run_params), intent(inout) :: setup
     ! Array for storing energy as a function of temperature
@@ -32,13 +57,15 @@ module initialise
     allocate(av_acceptance_of_T(setup%T_steps))
     ! Radial densities as a function of temperature
     allocate(av_rho_of_T(setup%n_species, setup%n_species, &
-                     setup%radial_d, setup%T_steps))
+                     setup%wc_range, setup%T_steps))
     av_rho_of_T = 0.0_real64
   end subroutine initialise_global_arrays
 
-  !---------------------------------------!
-  ! Subroutine to set up function pointer !
-  !---------------------------------------!
+  !--------------------------------------------------------------------!
+  ! Subroutine to initialise function pointers                         !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
   subroutine initialise_function_pointers(setup)
     type(run_params), intent(inout) :: setup
 
@@ -46,7 +73,9 @@ module initialise
 
     ! Setup functions used in calculations
     if(trim(setup%lattice) == 'simple_cubic') then
-      if (setup%n_shells .eq. 1) then
+      setup%n_basis = 1
+      setup%n_atoms = 8*setup%n_1*setup%n_2*setup%n_3
+      if (setup%interaction_range .eq. 1) then
         setup%nbr_energy => simple_cubic_energy_1shells
       else
         print*, 'Unsupported number of shells'
@@ -55,20 +84,22 @@ module initialise
       setup%rdm_site => simple_cubic_random_site
       setup%rdm_nbr => simple_cubic_random_nbr
     else if(trim(setup%lattice) == 'bcc') then
+      setup%n_basis = 1
+      setup%n_atoms = 2*setup%n_1*setup%n_2*setup%n_3
       setup%lattice_vectors = reshape( (/-0.5, 0.5, 0.5,     &
                                           0.5,-0.5, 0.5,     &
                                           0.5, 0.5,-0.5  /), &
                                        (/ 3, 3 /))
       setup%basis_vectors   = (/ 0.0, 0.0, 0.0 /)
-      if (setup%n_shells .eq. 1) then
+      if (setup%interaction_range .eq. 1) then
         setup%nbr_energy => bcc_energy_1shells
-      else if (setup%n_shells .eq. 2) then
+      else if (setup%interaction_range .eq. 2) then
         setup%nbr_energy => bcc_energy_2shells
-      else if (setup%n_shells .eq. 3) then
+      else if (setup%interaction_range .eq. 3) then
         setup%nbr_energy => bcc_energy_3shells
-      else if (setup%n_shells .eq. 4) then
+      else if (setup%interaction_range .eq. 4) then
         setup%nbr_energy => bcc_energy_4shells
-      else if (setup%n_shells .eq. 5) then
+      else if (setup%interaction_range .eq. 5) then
         setup%nbr_energy => bcc_energy_5shells
       else
         print*, 'Unsupported number of shells'
@@ -76,41 +107,57 @@ module initialise
       end if
       setup%rdm_site => bcc_random_site
       setup%rdm_nbr => bcc_random_nbr
-!    else if(trim(setup%lattice) == 'fcc') then
-!      if (setup%n_shells .eq. 1) then
-!        setup%nbr_energy => fcc_energy_1shells
-!      else if (setup%n_shells .eq. 2) then
-!        setup%nbr_energy => fcc_energy_2shells
-!      else if (setup%n_shells .eq. 3) then
-!        setup%nbr_energy => fcc_energy_3shells
-!      else if (setup%n_shells .eq. 4) then
-!        setup%nbr_energy => fcc_energy_4shells
-!      else if (setup%n_shells .eq. 5) then
-!        setup%nbr_energy => fcc_energy_5shells
-!      else if (setup%n_shells .eq. 6) then
-!        setup%nbr_energy => fcc_energy_6shells
-!      else
-!        print*, 'Unsupported number of shells'
-!        stop
-!      end if
-!      setup%rdm_site => fcc_random_site
-!      setup%rdm_nbr => fcc_random_nbr
+    else if(trim(setup%lattice) == 'fcc') then
+      setup%n_basis = 1
+      setup%n_atoms = 4*setup%n_1*setup%n_2*setup%n_3
+      setup%lattice_vectors = reshape( (/ 0.0, 0.5, 0.5,     &
+                                          0.5, 0.0, 0.5,     &
+                                          0.5, 0.5, 0.0  /), &
+                                       (/ 3, 3 /))
+      setup%basis_vectors   = (/ 0.0, 0.0, 0.0 /)
+      if (setup%interaction_range .eq. 1) then
+        setup%nbr_energy => fcc_energy_1shells
+      else if (setup%interaction_range .eq. 2) then
+        setup%nbr_energy => fcc_energy_2shells
+      else if (setup%interaction_range .eq. 3) then
+        setup%nbr_energy => fcc_energy_3shells
+      else if (setup%interaction_range .eq. 4) then
+        setup%nbr_energy => fcc_energy_4shells
+      else if (setup%interaction_range .eq. 5) then
+        setup%nbr_energy => fcc_energy_5shells
+      else if (setup%interaction_range .eq. 6) then
+        setup%nbr_energy => fcc_energy_6shells
+      else
+        print*, 'Unsupported number of shells'
+        stop
+      end if
+      setup%rdm_site => fcc_random_site
+      setup%rdm_nbr => fcc_random_nbr
     ! Bomb if we ask for a lattice type we don't have!
     else
       print*, 'Lattice type not yet implemented!'
       stop
     end if
-    ! Are we swapping neighbours or on the whole lattice?
-    if (setup%nbr_swap) then
-      setup%mc_step => monte_carlo_step_nbr
-    else
-      setup%mc_step => monte_carlo_step_lattice
-    end if
   end subroutine initialise_function_pointers
 
-  !----------------------------------------------------!
-  ! Subroutine to allocate memory for rank 1 processor !
-  !----------------------------------------------------!
+  !--------------------------------------------------------------------!
+  ! Subroutine to allocate memory for storing interaction parameters   !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
+  subroutine initialise_interaction(setup)
+    type(run_params), intent(inout) :: setup
+
+    ! Allocate space for atom-atom interchange parameters
+    allocate(V_ex(setup%n_species, setup%n_species, setup%interaction_range))
+
+  end subroutine initialise_interaction
+
+  !--------------------------------------------------------------------!
+  ! Subroutine to allocate memory for all processors                   !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
   subroutine initialise_local_arrays(setup)
     type(run_params), intent(inout) :: setup
     ! Array for storing energy as a function of temperature
@@ -121,43 +168,59 @@ module initialise
     allocate(acceptance_of_T(setup%T_steps))
     ! Radial densities as a function of temperature
     allocate(rho_of_T(setup%n_species, setup%n_species, &
-                     setup%radial_d, setup%T_steps))
+                     setup%wc_range, setup%T_steps))
     rho_of_T = 0.0_real64
     ! On-shell distances
-    allocate(shells(setup%radial_d))
+    allocate(shells(setup%wc_range))
     shells = 0.0_real64
     ! Array for storing temperatures
     allocate(temperature(setup%T_steps))
-    ! Allocate space for exchange parameters
-    allocate(V_ex(setup%n_species, setup%n_species, setup%n_shells))
     ! Allocate array for storing configuration
     allocate(config(setup%n_basis, 2*setup%n_1, 2*setup%n_2, 2*setup%n_3))
   end subroutine initialise_local_arrays
 
-  !-----------------------------------!
-  ! Subroutine to clean up at the end !
-  !-----------------------------------!
-  subroutine local_clean_up()
+  !--------------------------------------------------------------------!
+  ! Subroutine to clean up interaction array                           !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
+  subroutine clean_up_interaction(setup)
+    type(run_params), intent(inout) :: setup
+
     deallocate(V_ex)
+
+  end subroutine clean_up_interaction
+
+  !--------------------------------------------------------------------!
+  ! Subroutine to clean up local arrays                                !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
+  subroutine local_clean_up(setup)
+    type(run_params), intent(inout) :: setup
     deallocate(rho_of_T)
     deallocate(shells)
     deallocate(temperature)
     deallocate(config)
     deallocate(energies_of_T, C_of_T, acceptance_of_T)
+    deallocate(setup%species_names, setup%species_concentrations)
   end subroutine local_clean_up
 
-  !-----------------------------------!
-  ! Subroutine to clean up at the end !
-  !-----------------------------------!
+  !--------------------------------------------------------------------!
+  ! Subroutine to clean up average arrays                              !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
   subroutine global_clean_up()
     deallocate(av_rho_of_T)
     deallocate(av_energies_of_T, av_C_of_T, av_acceptance_of_T)
   end subroutine global_clean_up
 
-  !---------------------------------------------!
-  ! Function to setup lattice. We put zeroes on !
-  ! every 'site' not part of the lattice.       !
-  !---------------------------------------------!
+  !--------------------------------------------------------------------!
+  ! Subroutine to initialise the grid with a random configuration      !
+  !                                                                    !
+  ! C. D. Woodgate,  Warwick                                      2023 !
+  !--------------------------------------------------------------------!
   subroutine initial_setup(setup, config)
     type(run_params) :: setup
     integer(int16), allocatable, dimension(:,:,:,:) :: config
@@ -188,12 +251,9 @@ module initialise
     n_species = int(setup%n_species, kind=int16)
 
     do i=1, n_species-1
-      species_count(i) = floor(real(n_sites)*setup%species_cs(i))
+      species_count(i) = floor(real(n_sites)*setup%species_concentrations(i))
     end do
     species_count(n_species) = n_sites - sum(species_count(1:(n_species-1)))
-
-    ! Setup the concentrations array
-    call set_concentrations(setup)
 
     ! Set configuration to be zero
     config = 0_int16
@@ -211,8 +271,8 @@ module initialise
               ! Loop over species
               do l=1, n_species
                 ! Decide which species to sit on that site
-                if ((rand .ge. sum(setup%species_cs(0:(l-1)))) .and. &
-                    (rand .le. sum(setup%species_cs(0:l)))) then
+                if ((rand .ge. sum(setup%species_concentrations(0:(l-1)))) .and. &
+                    (rand .le. sum(setup%species_concentrations(0:l)))) then
                   if (check(l) .lt. species_count(l)) then
                     config(1,i,j,k) = l
                     check(l) = check(l) + 1
@@ -235,8 +295,8 @@ module initialise
               ! Loop over species
               do l=1, n_species
                 ! Decide which species to sit on that site
-                if ((rand .ge. sum(setup%species_cs(0:(l-1)))) .and. &
-                    (rand .le. sum(setup%species_cs(0:l)))) then
+                if ((rand .ge. sum(setup%species_concentrations(0:(l-1)))) .and. &
+                    (rand .le. sum(setup%species_concentrations(0:l)))) then
                   if (check(l) .lt. species_count(l)) then
                     if( modulo(k, 2) == 1) then
                       config(1,2*i-1, 2*j-1,k) = l
@@ -252,7 +312,6 @@ module initialise
         end do ! j
       end do ! k
     ! Deal with fcc case
-    ! Work in progress
     else if (trim(setup%lattice) == 'fcc') then
       ! Loop over lattice sites
       do k=1, grid_dims(4)
@@ -265,8 +324,8 @@ module initialise
               ! Loop over species
               do l=1, n_species
                 ! Decide which species to sit on that site
-                if ((rand .ge. sum(setup%species_cs(0:(l-1)))) .and. &
-                    (rand .le. sum(setup%species_cs(0:l)))) then
+                if ((rand .ge. sum(setup%species_concentrations(0:(l-1)))) .and. &
+                    (rand .le. sum(setup%species_concentrations(0:l)))) then
                   if (check(l) .lt. species_count(l)) then
                     if( modulo(k, 2) == 1) then
                       if( modulo(j,2) == 1) then
@@ -294,63 +353,6 @@ module initialise
       stop
     end if
 
-  print*, new_line('a'), 'Finished initial setup', new_line('a')
   end subroutine initial_setup
 
-  subroutine set_concentrations(parameters)
-    type(run_params) :: parameters
-    integer :: i
-
-    parameters%species_cs = 0.0_real64
-
-    ! Setup array of concentrations
-    ! Sloppy coding, I know, but keeps input strictly to input file
-    ! Currently handles up to six species
-    if (parameters%n_species .eq. 2) then
-      parameters%species_cs(1) = parameters%c_a
-      parameters%species_cs(2) = 1.0_real64 - parameters%c_a
-    else if (parameters%n_species .eq. 3) then
-      parameters%species_cs(1) = parameters%c_a
-      parameters%species_cs(2) = parameters%c_b
-      parameters%species_cs(3) = 1.0_real64 - parameters%c_a&
-                                            - parameters%c_b
-    else if (parameters%n_species .eq. 4) then
-      parameters%species_cs(1) = parameters%c_a
-      parameters%species_cs(2) = parameters%c_b
-      parameters%species_cs(3) = parameters%c_c
-      parameters%species_cs(4) = 1.0_real64 - parameters%c_a&
-                                            - parameters%c_b&
-                                            - parameters%c_c
-    else if (parameters%n_species .eq. 5) then
-      parameters%species_cs(1) = parameters%c_a
-      parameters%species_cs(2) = parameters%c_b
-      parameters%species_cs(3) = parameters%c_c
-      parameters%species_cs(4) = parameters%c_d
-      parameters%species_cs(5) = 1.0_real64 - parameters%c_a&
-                                            - parameters%c_b&
-                                            - parameters%c_c&
-                                            - parameters%c_d
-    else if (parameters%n_species .eq. 6) then
-      parameters%species_cs(1) = parameters%c_a
-      parameters%species_cs(2) = parameters%c_b
-      parameters%species_cs(3) = parameters%c_c
-      parameters%species_cs(4) = parameters%c_d
-      parameters%species_cs(5) = parameters%c_e
-      parameters%species_cs(6) = 1.0_real64 - parameters%c_a&
-                                            - parameters%c_b&
-                                            - parameters%c_c&
-                                            - parameters%c_d&
-                                            - parameters%c_e
-    else
-      stop 'Unsupported number of species.'
-    end if
-
-    ! Error handling
-    do i=1, parameters%n_species
-      if ((parameters%species_cs(i) .lt. 0.0_real64) .or. &
-          (parameters%species_cs(i) .gt. 1.0_real64)) then
-        stop 'Bad concentrations specified'
-      end if
-    end do
-  end subroutine set_concentrations
 end module initialise
