5a0e94ba6b8b160861e4b010dab6a44ee8b8b5e0
Chris Woodgate <c.woodgate@warwick.ac.uk>
2024-07-18 15:40:08 +0100

    Altering displayed version flag.

--- src/io.f90
+++ src/io.f90
@@ -30,7 +30,7 @@ module io
     call date_and_time(date=date,time=time)
 
     write(6,'(/,72("="))')
-    write(6,'(18x,"Bonte Warlo Version 0.1.2, 08.01.24")')
+    write(6,'(18x,"Bonte Warlo Version 0.2.1, 18.07.24")')
     write(6,'(72("-"))')
 
     if (point .eq. 's') then
