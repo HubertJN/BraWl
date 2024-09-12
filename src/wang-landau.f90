1d66e2ef5320d0656986c46c22cb53b2399fd5e1
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-09-12 11:30:05 +0100

    first iteration of functional wang landau code

--- src/wang-landau.f90
+++ src/wang-landau.f90
@@ -1,203 +1,246 @@
 !----------------------------------------------------------------------!
-! wang-landau.f90                                                      !
-!                                                                      !
 ! Wang-Landau module                                                   !
 !                                                                      !
-! C. D. Woodgate,  Warwick                                        2024 !
+! H. Naguszewski, Warwick                                         2024 !
 !----------------------------------------------------------------------!
-module wang_landau
 
+module wang_landau
   use initialise
   use kinds
   use shared_data
-  use io
-  use comms
   use c_functions
-  use energetics
   use random_site
-  use analytics
-  use write_netcdf
-  use write_xyz
-  use write_diagnostics
   use metropolis
-  
+  use mpi
+
   implicit none
 
   contains
 
-  !--------------------------------------------------------------------!
-  ! Examples of how to do basic things                                 !
-  !                                                                    !
-  ! C. D. Woodgate,  Warwick                                      2024 !
-  !--------------------------------------------------------------------!
-  subroutine examples(setup, my_rank)
+  subroutine wl_main(setup, wl_setup, my_rank)
+      ! Rank of this processor
+      integer, intent(in) :: my_rank
+      integer :: ierror, num_proc
 
-    ! Rank of this processor
-    integer, intent(in) :: my_rank
-
-    ! Arrays for storing data
-    type(run_params) :: setup
-  
-    ! Integers used in calculations
-    integer :: i,j, div_steps, accept, n_save
+      ! Arrays for storing data
+      type(run_params) :: setup
+      type(tmmc_params) :: wl_setup
     
-    ! Temperature and temperature steps
-    real(real64) :: beta, temp, sim_temp, current_energy, step_E,     &
-                    step_Esq, C, acceptance
-  
-    ! Name of file for grid state and xyz file at this temperature
-    character(len=36) :: xyz_file
-
-    ! Name of file for writing diagnostics at the end
-    character(len=43) :: diagnostics_file
-  
-    ! Name of file for writing radial densities at the end
-    character(len=37) :: radial_file
-
-    ! This is for calculating radial density functions later
-    ! (It populates the array "shells".)
-    call lattice_shells(setup, shells, config)
-
-    ! Are we swapping just nearest-neighbours or 
-    ! allowing any pair of lattice sites?
-    if (setup%nbr_swap) then
-      setup%mc_step => monte_carlo_step_nbr
-    else
-      setup%mc_step => monte_carlo_step_lattice
-    end if
-
-    !----------!
-    ! EXAMPLES !
-    !----------!
-  
-    !--------------------------------------------!
-    ! 1. Initialise a random grid and display it !
-    !--------------------------------------------!
-
-    ! Set up the initial state of the grid (random occupancies)
-    ! on this rank. (Each rank works on different grid currently.)
-    call initial_setup(setup, config)
-
-    if(my_rank == 0) then
-      print*, ' '
-      print*, ' This is what the grid initially looks like: '
-      print*, ' '
-    end if
-  
-    ! You can see what it looks like (sort of) using this routine
-    call display_grid(config)
-
-    !---------------------------------!
-    ! 2. Perform a Metropolis MC step !
-    !---------------------------------!
-
-    ! Would normally use j to loop over temperatures
-    j = 1
-
-    ! Work out the temperature and corresponding beta
-    temp = setup%T + real(j-1, real64)*setup%delta_T
-    sim_temp = temp*k_b_in_Ry
-    beta = 1.0_real64/sim_temp
+      ! Integers used in calculations
+      integer :: i,j,k, ierr, accept, bins
+      
+      ! Temperature and temperature steps
+      real(real64) :: temp, acceptance, beta, flatness
     
-    do while (accept .lt. 0.5_real64)
-          accept = setup%mc_step(config, beta)
-    end do
+      ! wl variables and arrays
+      real(real64) :: bin_width, energy_to_ry, target_energy, wl_f
+      real(real64), allocatable :: bin_edges(:), wl_hist, wl_logdos
+      logical :: first_reset
 
-    if(my_rank == 0) then
-      print*, ' '
-      print*, ' This is what the grid looks like after one successful step: '
-      print*, ' '
-    end if
+      ! Allocate arrays
+      bins = wl_setup%bins
 
-    call display_grid(config)
+      allocate(bin_edges(bins+1))
+      allocate(wl_hist(bins))
+      allocate(wl_logdos(bins))
+
+      ! Set temperature
+      temp = setup%T*k_b_in_Ry
+
+      ! Conversion meV/atom to Rydberg
+      energy_to_ry=setup%n_atoms/(eV_to_Ry*1000)
+
+      bin_width = (wl_setup%energy_max - wl_setup%energy_min)/real(wl_setup%bins)*energy_to_ry
+      do i=1, bins+1
+          bin_edges(i) = wl_setup%energy_min*energy_to_ry + (i-1)*bin_width
+      end do
+
+      wl_hist = 0.0_real64; wl_logdos = 0.0_real64; wl_f = 0.05_real64
+      flatness = 0.0_real64; first_reset = .False.
+      !---------------------------------!
+
+      ! Set up the lattice
+      call initial_setup(setup, config)
   
-    !---------------------------------------------------!
-    ! 3. Get the energy associated with a configuration !
-    !---------------------------------------------------!
-
-    current_energy = setup%full_energy(config)
-
-    if(my_rank == 0) then
-      print*, ' '
-      print*, ' The energy of that configuration is: ', current_energy, ' Ry'
-      print*, ' or: ', current_energy*13.606*1000.0, ' meV'
-      print*, ' or: ', current_energy*13.606*1000.0/setup%n_atoms, ' meV/atom'
-      print*, ' '
-    end if
-
-    !------------------------------------!
-    ! 4. Run some kind of equillibration !
-    !------------------------------------!
-    n_save=floor(real(setup%mc_steps)/real(setup%sample_steps))
-    div_steps = setup%mc_steps/1000
-
-    do i=1, setup%mc_steps
+      call lattice_shells(setup, shells, config)
     
-      ! Make one MC move
-      accept = setup%mc_step(config, beta)
-  
-      acceptance = acceptance + accept
-
-      ! Write percentage progress to screen
-      if (mod(i, setup%sample_steps) .eq. 0) then
-        current_energy = setup%full_energy(config)
-        step_E   = step_E + current_energy
-        step_Esq = step_Esq + current_energy**2
+      ! Are we swapping neighbours or on the whole lattice?
+      if (setup%nbr_swap) then
+        setup%mc_step => monte_carlo_step_nbr
+      else
+        setup%mc_step => monte_carlo_step_lattice
       end if
+
+      if(my_rank == 0) then
+        write(6,'(/,72("-"),/)')
+        write(6,'(24("-"),x,"Commencing Simulation!",x,24("-"),/)')
+        print*, "Number of atoms", setup%n_atoms
+      end if
+
+      !---------!
+      ! Burn in !
+      !---------!
+      beta = 1.0_real64/temp
+      if (wl_setup%burn_in) then
+          call wl_burn_in(setup, wl_setup, config, temp, target_energy, trans_matrix, bin_edges, bins)
+      end if
+
+      !--------------------!
+      ! Target Temperature !
+      !--------------------!
+      do while (wl_f > wl_setup%tolerance)
+          acceptance = run_wl_sweeps(setup, wl_setup, config, bins, bin_edges, bins_mpi, &
+          bin_edges_mpi, wl_hist, wl_logdos, wl_f)
+
+          flatness = 100.0_real64*minval(wl_hist)/(sum(wl_hist)/size(wl_hist))
+
+          if (minval(wl_hist) > 10.0_real64) then
     
-    end do
+            if (first_reset == .False.) then
+              first_reset = .True.
+              wl_hist = 0.0_real64
 
-    ! Store the average energy per atom at this temperature
-    energies_of_T(j) = step_E/n_save/setup%n_atoms
+            else
+              !Check if we're 80% flat
+              if flatness > 80.0_real64 then
   
-    ! Heat capacity (per atom) at this temperature
-    C = (step_Esq/n_save - (step_E/n_save)**2)/(sim_temp*temp)/setup%n_atoms
+                  !Reset the histogram
+                  wl_hist = 0.0_real64
+                  wl_f = wl_f * 0.5_real64
+  
+                  !Reduce f
+                  wl_logdos = wl_logdos - minval(wl_logdos)
+              end if
+            end if
 
-    ! Acceptance rate at this temperature
-    acceptance_of_T(j) = acceptance/real(setup%mc_steps)
-  
-    ! Store the specific heat capacity at this temperature
-    C_of_T(j) = C
-    
-    if (my_rank ==0) then
-      ! Write that we have completed a particular temperature
-      write(6,'(a,f7.2,a)',advance='yes') &
-      " Temperature ", temp, " complete on process 0."
-      write(6,'(a,f7.2,a)',advance='yes') &
-      " Internal energy was ", 13.606_real64*1000*energies_of_T(j), " meV/atom"
-      write(6,'(a,f9.4,a)',advance='yes') &
-      " Heat capacity was ", C_of_T(j)/k_B_in_Ry, " kB/atom"
-      write(6,'(a,f7.2,a,/)',advance='yes') &
-      " Swap acceptance rate was ", 100.0*acceptance_of_T(j), "%"
-    end if
-    
-    !------------------------------------!
-    ! 5. Write the grid to a .xyz file   !
-    !------------------------------------!
+          if(my_rank == 0) then
+              write(6,'(a,i0,a,f6.2,a,f6.2,a)',advance='yes') "Weight Update ", i, ": Accepted ", &
+               (acceptance/(wl_setup%mc_sweeps*setup%n_atoms)*100.0), "% of Monte Carlo moves. Time taken: ", end-start, "s"
+          end if
+      end do
+      
+      if (my_rank == 0) then
+          ! Write output files
+          call ncdf_writer_1d("wl_dos_bins.dat", ierr, bin_edges)
 
-    write(xyz_file, '(A11 I3.3 A12 I4.4 F2.1 A4)') 'grids/proc_', &
-    my_rank, 'config_at_T_', int(temp), temp-int(temp),'.xyz'
+          call ncdf_writer_1d("wl_dos.dat", ierr, wl_dos)
+      end if
+      
+      if(my_rank == 0) then
+          write(*, *)
+          write(6,'(25("-"),x,"Simulation Complete!",x,25("-"))')
+      end if
   
-    ! Write xyz file
-    call xyz_writer(xyz_file, config, setup)
-  
-    !----------------------------------------------------------------!
-    ! 6. Compute the Warren-Cowley ASRO parameters and write to file !
-    !----------------------------------------------------------------!
+  end subroutine wl_main    
 
-    ! Compute the radial densities at the end of this temperature
-    call radial_densities(setup, config, setup%wc_range, shells, rho_of_T, j)
-  
-    write(radial_file, '(A22 I3.3 A12)') 'radial_densities/proc_', my_rank, '_rho_of_T.nc'
+  integer function bin_index(energy, bin_edges, bins) result(index)
+      integer, intent(in) :: bins
+      real(real64), intent(in) :: energy
+      real(real64), dimension(:), intent(in) :: bin_edges
+      real(real64) :: bin_range
 
-    ! Write the radial densities to file
-    call ncdf_radial_density_writer(radial_file, rho_of_T, &
-                                  shells, temperature, energies_of_T, setup)
+      bin_range = bin_edges(bins+1) - bin_edges(1)
+      index = int(((energy - bin_edges(1))/(bin_range))*real(bins)) + 1
+  end function bin_index
 
-    if(my_rank == 0) then
-      write(6,'(25("-"),x,"Simulation Complete!",x,25("-"))')
-    end if
+  function run_wl_sweeps(setup, wl_setup, config, bins, bin_edges, bins_mpi, &
+      bin_edges_mpi, wl_hist, wl_logdos, wl_f) result(acceptance)
+      integer(int16), dimension(:,:,:,:) :: config
+      class(run_params), intent(in) :: setup
+      class(tmmc_params), intent(in) :: wl_setup
+      integer, intent(in) :: bins, bins_mpi
+      real(real64), dimension(:), intent(in) :: bin_edges, bin_edges_mpi
+      real(real64), dimension(:), intent(inout) :: wl_hist, wl_logdos
+      real(real64) , intent(in) :: temp, wl_f
+
+      integer, dimension(4) :: rdm1, rdm2
+      real(real64) :: e_swapped, e_unswapped, delta_e
+      integer :: acceptance, i, ibin, jbin, ibin_mpi, jbin_mpi
+
+      ! Establish total energy before any moves
+      e_unswapped = setup%full_energy(config)
+
+      acceptance = 0.0_real64
+
+      do i=1, wl_setup%mc_sweeps*setup%n_atoms
   
-  end subroutine examples
+          ! Make one MC trial
+          ! Generate random numbers
+          rdm1 = setup%rdm_site()
+          rdm2 = setup%rdm_site()
+
+          call pair_swap(config, rdm1, rdm2)
+  
+          e_swapped = setup%full_energy(config)
+
+          ibin = bin_index(e_unswapped, bin_edges, bins)
+          jbin = bin_index(e_swapped, bin_edges, bins)
+
+          ibin_mpi = bin_index(e_unswapped, bin_edges_mpi, bins_mpi)
+          jbin_mpi = bin_index(e_swapped, bin_edges_mpi, bins_mpi)
+
+          ! Only compute energy change if within limits where V is defined
+          if (jbin > 0 .and. jbin < bins+1) then
+              ! Only compute move if within mpi bin energy range
+              if (jbin_mpi > 0 .and. jbin_mpi < bins_mpi+1) then
+                  ! Add change in V into diff_energy
+                  delta_e = e_swapped - e_unswapped
+
+                  ! Accept or reject move
+                  if (genrand() .lt. m.exp(wl_logdos[ibin]-wl_logdos[jbin])) then
+                      acceptance = acceptance + 1
+                      e_unswapped = e_swapped
+                  else
+                      call pair_swap(config, rdm1, rdm2)
+                  end if
+              else
+                  call pair_swap(config, rdm1, rdm2)
+              end if
+              wl_hist(ibin) = wl_hist(ibin) + 1
+              wl_logdos(jbin) = wl_logdos(jbin) + wl_f
+          else
+              ! reject and reset
+              call pair_swap(config, rdm1, rdm2)
+          end if
+      end do
+
+  end function run_wl_sweeps
+
+  subroutine wl_burn_in(setup, wl_setup, config, temp, bin_edges, bins)
+      integer(int16), dimension(:,:,:,:) :: config
+      class(run_params), intent(in) :: setup
+      class(tmmc_params), intent(in) :: wl_setup
+      real(real64) , intent(in) :: temp
+
+      integer, dimension(4) :: rdm1, rdm2
+      real(real64) :: e_swapped, e_unswapped, delta_e, beta
+      integer :: i, ibin, jbin
+
+      ! Establish total energy before any moves
+      e_unswapped = setup%full_energy(config)
+
+      do i=1, wl_setup%burn_in_sweeps*setup%n_atoms
+          ! Make one MC trial
+          ! Generate random numbers
+          rdm1 = setup%rdm_site()
+          rdm2 = setup%rdm_site()
+
+          call pair_swap(config, rdm1, rdm2)
+  
+          e_swapped = setup%full_energy(config)
+
+          ibin = bin_index(e_unswapped, bin_edges, bins)
+          jbin = bin_index(e_swapped, bin_edges, bins)
+
+          delta_e = e_swapped - e_unswapped
+
+          ! Accept or reject move
+          if (delta_e < 0) then
+              e_unswapped = e_swapped
+          else 
+            call pair_swap(config, rdm1, rdm2)
+          end if
+      end do
+  end subroutine wl_burn_in
 
 end module wang_landau
