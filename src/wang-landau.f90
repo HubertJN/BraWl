b58075dafbd8fa383a90c07bdfa01c1281ba51a1
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-10-09 11:08:43 +0100

    MPI functional

--- src/wang-landau.f90
+++ src/wang-landau.f90
@@ -41,13 +41,13 @@ module wang_landau
 
       ! window variables
       integer, allocatable :: window_indices(:,:)
-      integer :: num_windows
+      integer :: num_windows, num_walkers
       real(real64) :: window_width
 
       ! mpi variables
       real(real64) :: start, end, time_max, time_min
       integer :: mpi_bins, mpi_start_idx, mpi_end_idx, bin_overlap, mpi_index
-      real(real64) :: mpi_width, scale_factor, scale_count
+      real(real64) :: mpi_width, scale_factor, scale_count, wl_logdos_min
       real(real64), allocatable :: mpi_bin_edges(:), mpi_wl_hist(:), wl_logdos_buffer(:)
 
       ! Minimum value in array to be considered
@@ -59,13 +59,13 @@ module wang_landau
       ! Allocate arrays
       bins = wl_setup%bins
       num_windows = wl_setup%num_windows
-      if (MOD(num_windows, num_proc) /= 0) then
+      num_walkers = num_proc/num_windows
+      if (MOD(num_proc, num_windows) /= 0) then
         if(my_rank == 0) then
           write(6,'(72("~"))')
           write(6,'(5("~"),x,"Error: Number of MPI processes not divisible by num_windows",x,6("~"))')
           write(6,'(72("~"))')
         end if
-        call comms_wait()
         call MPI_FINALIZE(ierror)
         call EXIT(0)
       end if
@@ -80,7 +80,7 @@ module wang_landau
       
       call comms_wait()
 
-      mpi_index = my_rank/num_proc + 1
+      mpi_index = my_rank/num_walkers + 1
       mpi_start_idx = window_indices(mpi_index,1)
       mpi_end_idx = window_indices(mpi_index,2)
       mpi_bins = mpi_end_idx - mpi_start_idx + 1
@@ -88,9 +88,7 @@ module wang_landau
       allocate(bin_edges(bins+1))
       allocate(wl_hist(bins))
       allocate(wl_logdos(bins))
-      if (my_rank == 0) then
-        allocate(wl_logdos_buffer(bins))
-      end if
+      allocate(wl_logdos_buffer(bins))
 
       ! MPI arrays
       allocate(mpi_bin_edges(mpi_bins+1))
@@ -103,7 +101,7 @@ module wang_landau
       energy_to_ry=setup%n_atoms/(eV_to_Ry*1000)
 
       window_width = (wl_setup%energy_max - wl_setup%energy_min)/real(num_windows)*energy_to_ry
-      target_energy = wl_setup%energy_min*energy_to_ry + mpi_width*(my_rank + 0.5)
+      target_energy = wl_setup%energy_min*energy_to_ry + window_width*(mpi_index - 1 + 0.5)
 
       j = 1
       bin_width = (wl_setup%energy_max - wl_setup%energy_min)/real(wl_setup%bins)*energy_to_ry
@@ -118,7 +116,7 @@ module wang_landau
       wl_hist = 0.0_real64; wl_logdos = 0.0_real64; wl_f = wl_setup%wl_f; wl_f_prev = wl_f
       flatness = 0.0_real64; first_reset = .False.; hist_reset = .True.
 
-      mpi_wl_hist = 0.0_real64
+      mpi_wl_hist = 0.0_real64; wl_logdos_buffer = 0.0_real64
       !---------------------------------!
 
       ! Set up the lattice
@@ -176,6 +174,8 @@ module wang_landau
         flatness = 100.0_real64*minval(mpi_wl_hist, MASK=(mpi_wl_hist > min_val))/(sum(mpi_wl_hist, &
         MASK=(mpi_wl_hist > min_val))/count(MASK=(mpi_wl_hist > min_val)))
 
+        !print*, my_rank, flatness
+
         if (first_reset .eqv. .False. .and. minval(mpi_wl_hist, MASK=(mpi_wl_hist > min_val)) > 100) then ! First reset after system had time to explore
           first_reset = .True.
           mpi_wl_hist = 0.0_real64
@@ -187,8 +187,49 @@ module wang_landau
             mpi_wl_hist = 0.0_real64
             hist_reset = .True.
             !Reduce f
-            wl_logdos = wl_logdos - minval(wl_logdos, MASK=(wl_logdos > min_val))
+            call MPI_ALLREDUCE(minval(wl_logdos, MASK=(wl_logdos > min_val)), wl_logdos_min, 1, MPI_DOUBLE_PRECISION, &
+            MPI_MIN, MPI_COMM_WORLD, ierror)
+            wl_logdos = wl_logdos - wl_logdos_min
             wl_logdos = wl_logdos * merge(0, 1, wl_logdos < 0.0_real64)
+
+            !Average DoS across walkers
+            do i=1, num_windows
+                if (mpi_index == i) then
+                  if (my_rank /= (i-1)*num_walkers) then
+                    !print*, my_rank, wl_logdos
+                    call MPI_Send(wl_logdos, bins, MPI_DOUBLE_PRECISION, (i-1)*num_walkers, i, MPI_COMM_WORLD, ierror)
+                    !print*, my_rank, "Sent to 1", (i-1)*num_walkers
+                  end if
+
+                  if (my_rank == (i-1)*num_walkers) then
+                    do j=1, num_walkers-1
+                      call MPI_Recv(wl_logdos_buffer, bins, MPI_DOUBLE_PRECISION, (i-1)*num_walkers+j, i, MPI_COMM_WORLD, &
+                      MPI_STATUS_IGNORE, ierror)
+                      !print*, my_rank, "Recieving from 1", (i-1)*num_walkers+j
+                      !print*, my_rank, wl_logdos
+                      wl_logdos = wl_logdos + wl_logdos_buffer
+                    end do
+                 end if
+
+                  call comms_wait()
+                  
+                  if (my_rank == (i-1)*num_walkers) then
+                    wl_logdos = wl_logdos/num_walkers
+                    !print*, my_rank, wl_logdos
+                    do j=1, num_walkers-1
+                      call MPI_Send(wl_logdos, bins, MPI_DOUBLE_PRECISION, (i-1)*num_walkers+j, i, MPI_COMM_WORLD, &
+                      ierror)
+                      !print*, my_rank, "Sent to 2", (i-1)*num_walkers+j
+                    end do
+                  else
+                    call MPI_Recv(wl_logdos_buffer, bins, MPI_DOUBLE_PRECISION, (i-1)*num_walkers, i, MPI_COMM_WORLD, &
+                    MPI_STATUS_IGNORE, ierror)
+                    !print*, my_rank, "Recieving from 2", (i-1)*num_walkers
+                    wl_logdos = wl_logdos_buffer
+                    !print*, my_rank, wl_logdos
+                  end if
+                end if
+            end do
           end if
         end if
 
@@ -204,42 +245,42 @@ module wang_landau
               write(6,'(a,i0,a,f6.2,a,f12.10,a,f6.2,a,f6.2,a)', advance='no'), "Rank: ", my_rank, " Flatness: ", flatness, &
               "% W-L F: ", wl_f_prev, " Time min:", time_min, "s Time max:", time_max, "s"
               wl_f_prev = wl_f
+              print*, wl_logdos_min
               write(*,*)
           end if
+          
+!          do i=1, num_proc-1 
+!            if (my_rank == i) then
+!              call MPI_Send(wl_logdos, bins, MPI_DOUBLE_PRECISION, 0, 0, MPI_COMM_WORLD, ierror)
+!              call MPI_Send(mpi_start_idx, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, ierror)
+!              call MPI_Send(mpi_end_idx, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, ierror)
+!            end if
+!            if (my_rank == 0) then
+!              call MPI_Recv(wl_logdos_buffer, bins, MPI_DOUBLE_PRECISION, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierror)
+!              call MPI_Recv(mpi_start_idx, 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierror)
+!              call MPI_Recv(mpi_end_idx, 1, MPI_INT, i, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierror)
+!              scale_factor = 0.0_real64
+!              scale_count = 0.0_real64
+!              do j=0, bin_overlap-1
+!                if (wl_logdos_buffer(mpi_start_idx+j) > min_val .and. wl_logdos(mpi_start_idx+j) > min_val) then
+!                  scale_factor = scale_factor + EXP(wl_logdos(mpi_start_idx+j))-EXP(wl_logdos_buffer(mpi_start_idx+j))
+!                  scale_count = scale_count + 1.0_real64
+!                end if
+!              end do
+!              scale_factor = scale_factor/scale_count
+!              do j=mpi_start_idx+bin_overlap, mpi_end_idx
+!                wl_logdos_buffer(j) = LOG(EXP(wl_logdos_buffer(j)) + scale_factor)
+!              end do
+!            end if
+!          end do
+!
+!          if (my_rank == 0) then
+!            ! Write output files
+!            call ncdf_writer_1d("wl_dos_bins.dat", ierr, bin_edges)
+!            call ncdf_writer_1d("wl_dos.dat", ierr, wl_logdos_buffer)
+!            call ncdf_writer_1d("wl_hist.dat", ierr, wl_hist)
+!          end if
         end if
-
-        do i=1, num_proc-1 
-          if (my_rank == i) then
-            call MPI_Send(wl_logdos, bins, MPI_DOUBLE_PRECISION, 0, 0, MPI_COMM_WORLD, ierror)
-            call MPI_Send(mpi_start_idx, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, ierror)
-            call MPI_Send(mpi_end_idx, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, ierror)
-          end if
-          if (my_rank == 0) then
-            call MPI_Recv(wl_logdos_buffer, bins, MPI_DOUBLE_PRECISION, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierror)
-            call MPI_Recv(mpi_start_idx, 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierror)
-            call MPI_Recv(mpi_end_idx, 1, MPI_INT, i, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierror)
-            scale_factor = 0.0_real64
-            scale_count = 0.0_real64
-            do j=0, bin_overlap-1
-              if (wl_logdos_buffer(mpi_start_idx+j) > min_val .and. wl_logdos(mpi_start_idx+j) > min_val) then
-                scale_factor = scale_factor + EXP(wl_logdos(mpi_start_idx+j))-EXP(wl_logdos_buffer(mpi_start_idx+j))
-                scale_count = scale_count + 1.0_real64
-              end if
-            end do
-            scale_factor = scale_factor/scale_count
-            do j=mpi_start_idx+bin_overlap, mpi_end_idx
-              wl_logdos(j) = LOG(EXP(wl_logdos_buffer(j)) + scale_factor)
-            end do
-          end if
-        end do
-  
-        if (my_rank == 0) then
-          ! Write output files
-          call ncdf_writer_1d("wl_dos_bins.dat", ierr, bin_edges)
-          call ncdf_writer_1d("wl_dos.dat", ierr, wl_logdos)
-          call ncdf_writer_1d("wl_hist.dat", ierr, wl_hist)
-        end if
-        
       end do
            
       if(my_rank == 0) then
