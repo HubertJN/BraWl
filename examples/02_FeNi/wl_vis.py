06df83f2628b6e37ce58c48f76dcf772ec93ef01
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-10-24 15:06:24 +0100

    changed plotting parameters

--- examples/02_FeNi/wl_vis.py
+++ examples/02_FeNi/wl_vis.py
@@ -12,6 +12,8 @@ filename = "wl_dos.dat"
 wl_logdos = nc.Dataset(filename)
 wl_logdos = np.array(wl_logdos["grid data"][:], dtype=np.float64)
 
+print(wl_logdos)
+
 filename = "wl_hist.dat"
 wl_hist = nc.Dataset(filename)
 wl_hist = np.array(wl_hist["grid data"][:], dtype=np.float64)
@@ -24,49 +26,40 @@ eV_to_meV = 1000
 J = 0.001
 N = 6
 
-orig_temp = 400
-start_temp = 200
-end_temp = 800
+orig_temp = 800
+start_temp = 100
+end_temp = 1000
 step_size = (end_temp-start_temp)/(75)
 temperatures = np.arange(start_temp, end_temp, step_size)
 
-plt.plot(wl_hist[wl_hist > 0])
-plt.show()
-
 # Reduce histogram such that all bins are non zero
-bin_width = bin_edges[1] - bin_edges[0]
-
-bin_energies = np.zeros(len(bin_edges[:-1]))
-for i, edge in enumerate(bin_edges[:-1]):
-    bin_energies[i] = edge + 0.5*bin_width
-
-bin_width = bin_edges[1] - bin_edges[0]
-
-bins = 32
-bin_width_con = (bin_edges[-1] - bin_edges[0])/(bins)
-bin_edges_con = np.arange(bin_edges[0], bin_edges[-1]+bin_width_con, bin_width_con)
-
-wl_logdos_con = np.zeros(len(bin_edges_con)-1)
-
-for i, edge in enumerate(bin_edges[:-1]):
-    bin_energy = edge + 0.5*bin_width
-    j = 0
-    while 1:
-        if bin_energy >= bin_edges_con[j] and bin_energy < bin_edges_con[j+1]:
-            wl_logdos_con[j] += np.exp(wl_logdos[i])
-            break
-        else:
-            j += 1
-
-print(wl_logdos)
-wl_logdos = np.log(wl_logdos_con)
-bin_edges = bin_edges_con
-print(wl_logdos)
+#bin_width = bin_edges[1] - bin_edges[0]
+#
+#bin_energies = np.zeros(len(bin_edges[:-1]))
+#for i, edge in enumerate(bin_edges[:-1]):
+#    bin_energies[i] = edge + 0.5*bin_width
+#
+#bin_width = bin_edges[1] - bin_edges[0]
+#
+#bins = 128
+#bin_width_con = (bin_edges[-1] - bin_edges[0])/(bins)
+#bin_edges_con = np.arange(bin_edges[0], bin_edges[-1]+bin_width_con, bin_width_con)
+#
+#bin_edges = bin_edges_con
 plt.plot(np.exp(wl_logdos))
 plt.show()
 # ------------------------------------------------
 
 bin_width = bin_edges[1] - bin_edges[0]
+
+prob = np.zeros(len(bin_edges)-1)
+beta = 1.0/(kb_ry*orig_temp)
+for ibin, edge in enumerate(bin_edges[:-1]):
+    bin_energy = edge + 0.5*bin_width
+    prob[ibin] = np.exp(wl_logdos[ibin]-beta*bin_energy)
+plt.plot(prob)
+plt.show()
+#exit()
 
 # Setup plots
 fig, [ax1, ax2, ax3, ax4] = plt.subplots(1, 4, figsize=(24, 7), constrained_layout=True)
@@ -104,10 +97,10 @@ for itemp, new_temp in enumerate(temperatures):
     # Reweight histogram
     for ibin, edge in enumerate(bin_edges[:-1]):
         bin_energy = edge + 0.5*bin_width
-        prob[ibin] = np.exp(wl_logdos[ibin]-beta*bin_energy)
+        prob[ibin] = np.exp(wl_logdos[ibin]-beta*bin_energy)  
 
     # Normalise
-    prob = prob/(np.sum(prob*bin_width))
+    prob = prob/(np.sum(prob))
 
     # Only plot every 5th histogram to avoid crowding the axes
     if itemp%4 == 0:
@@ -115,7 +108,7 @@ for itemp, new_temp in enumerate(temperatures):
         ax1.bar(bin_edges[:-1], prob, width=bin_width, align='edge', label=strlabel)
     
     # Mean energy
-    mean_energy = np.dot(bin_edges[:-1]+0.5*bin_width, prob)/np.sum(prob)
+    mean_energy = np.dot(bin_edges[:-1]+0.5*bin_width, prob)
     mean_energies[itemp] = mean_energy
 
     # Compute heat capacity using the histogram
@@ -126,7 +119,7 @@ for itemp, new_temp in enumerate(temperatures):
         
     heat_caps[itemp] = np.dot(msq_dev, prob)*bin_width/(kb_ry*new_temp**2)
 
-    entropy = (prob*bin_width)*np.log(prob*bin_width)
+    entropy = prob*np.log(prob)
     entropy[np.isnan(entropy)] = 0
     entropies[itemp] = -np.sum(entropy)
 
