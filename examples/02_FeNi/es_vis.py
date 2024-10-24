176e3509d2f72974f8cf0fb9a598a507f668bf43
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2024-10-24 15:05:57 +0100

    Changed plotting parameters

--- examples/02_FeNi/es_vis.py
+++ examples/02_FeNi/es_vis.py
@@ -8,12 +8,15 @@ filename = "energy_spectrum.dat"
 energy_spectrum = nc.Dataset(filename)
 energy_spectrum = np.array(energy_spectrum["grid data"][:], dtype=np.float64)
 
+print(energy_spectrum)
+
 ev_to_ry = 13.605693122
-n_atoms = 256
+n_atoms = 512
 
 energy_spectrum = energy_spectrum/(n_atoms/(ev_to_ry*1000))
 
 plt.figure()
-plt.eventplot(energy_spectrum, orientation='horizontal', colors='b', linewidths=0.1)
+plt.title("Energy min: {:.2f}, Energy max: {:.2f}".format(np.min(energy_spectrum),np.max(energy_spectrum)))
+plt.eventplot(energy_spectrum, orientation='horizontal', colors='b', linewidths=1)
 plt.xlabel("Energy (meV/atom)")
 plt.show()
