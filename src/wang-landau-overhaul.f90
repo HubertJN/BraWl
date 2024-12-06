8a1a684ca2f7e1d121df209ccac6f2f2e204c725
HubertJN <hubert.naguszewski@warwick.ac.uk>
2024-12-06 11:39:49 +0000

    Progress on burn_in function

--- src/wang-landau-overhaul.f90
+++ src/wang-landau-overhaul.f90
@@ -26,7 +26,7 @@ module wang_landau
     type(wl_params) :: wl_setup
 
     ! MPI variables
-    integer :: ierror, num_proc
+    integer :: ierror, mpi_process
 
     ! Convert input variables to simulation variables
     eV_per_atom_to_Ry = setup%n_atoms/(eV_to_Ry*1000) ! Conversion meV/atom to Rydberg
@@ -38,10 +38,10 @@ module wang_landau
     wl_setup%radial_samples = wl_setup%radial_samples/num_walkers
 
     ! Get number of MPI processes
-    call MPI_COMM_SIZE(MPI_COMM_WORLD, num_proc, ierror)
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, mpi_process, ierror)
 
     ! Number of MPI processes per MPI window
-    num_walkers = num_proc/wl_setup%num_windows
+    num_walkers = mpi_process/wl_setup%num_windows
 
     ! Obtain values for bin_edges array
     call energy_binning(wl_setup, bin_edges)
@@ -119,12 +119,14 @@ module wang_landau
   end subroutine energy_binning
 
   ! Subroutine that generates non-uniform bin distribution for MPI windows
-  subroutine divide_range(wl_setup, window_intervals)
+  ! and distributes available MPI processes
+  subroutine divide_range(wl_setup, mpi_process, window_intervals, window_rank_index)
     ! Subroutine input
     type(wl_params) :: wl_setup
+    integer, intent(in) :: mpi_process
 
     ! Subroutine output
-    integer, dimension(:, :), intent(out) :: window_intervals
+    integer, dimension(:, :), intent(out) :: window_intervals, window_rank_index
 
     ! Loop indices
     integer :: i
@@ -147,6 +149,17 @@ module wang_landau
     do i = 2, num_intervals
       window_intervals(i-1,2) = INT(FLOOR(factor*(i**power-1)+1))
       window_intervals(i,1) = INT(FLOOR(factor*(i**power-1)+1) + 1)
+    end do
+
+    ! Distribute MPI processes
+    window_rank_index(1,1) = 0
+    window_rank_index(wl_setup%num_windows, 2) = mpi_process
+    do i = 2, wl_setup%num_windows
+      window_rank_index(i,1) = INT(FLOOR(mpi_process/wl_setup%num_windows))*(i-1)
+      window_rank_index(i-1,2) = window_rank_index(i,1) - 1
+    end do
+    do i = 1, MOD(mpi_process, wl_setup%num_windows)
+      window_rank_index(i+1) = window_rank_index(i+1) + i
     end do
   end subroutine divide_range
 
@@ -179,3 +192,67 @@ module wang_landau
     mpi_bins = window_indices(mpi_index, 2) - window_indices(mpi_index, 1) + 1
 
   end subroutine mpi_arrays
+
+  ! Subroutine to burn-in MPI windows
+  subroutine burn_in(setup, wl_setup, config, my_rank, mpi_index, window_rank_index, &
+                     window_min_e, window_max_e)
+    ! Subroutine input
+
+    ! Target energy
+    target_energy = (window_min_e + window_max_e)/2.0_real64
+
+    ! Initiale configuration energy
+    e_unswapped = congif%full_energy(config)
+
+    ! Non-blocking MPI receive
+    call MPI_IRECV(stop_burn_in, 1, MPI_LOGICAL, MPI_ANY_SOURCE, mpi_index, MPI_COMM_WORLD, request, ierr)
+
+    do while(.True.)
+      ! Check if MPI message received
+      call MPI_TEST(request, flag, MPI_STATUS_IGNORE, ierr)
+
+      ! Stop burn if other rank in window is burnt in
+      ! or if burnt in send configuration to rest of window
+      if (stop_burn_in .eqv. .True.) then
+        call MPI_RECV(config, SIZE(config), MPI_SHORT, MPI_ANY_SOURCE, mpi_index, MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierr) ! Check if you can put an array of accepted values in the source variable
+        exit
+      else if (e_unswapped > window_min_e .and. e_unswapped < window_max_e) then
+        stop_burn_in = .True.
+        call MPI_CANCEL(request, ierr)
+        call MPI_REQUEST_FREE(request, ierr)
+        do rank=window_rank_index(mpi_index, 1), window_rank_index(mpi_index, 2)
+          call MPI_SEND(stop_burn, 1, MPI_LOGICAL, rank, mpi_index, MPI_COMM_WORLD, ierr)
+          call MPI_SEND(config, SIZE(config), MPI_SHORT, rank, mpi_index, MPI_COMM_WORLD, ierr)
+        end do
+      end if
+
+      ! Make one MC trial
+      ! Generate random numbers
+      rdm1 = setup%rdm_site()
+      rdm2 = setup%rdm_site()
+
+      ! Get what is on those sites
+      site1 = config(rdm1(1), rdm1(2), rdm1(3), rdm1(4))
+      site2 = config(rdm2(1), rdm2(2), rdm2(3), rdm2(4))
+
+      ! Only caclulate proceed for sites with different elements
+      if (site1 /= site2) then
+        call pair_swap(config, rdm1, rdm2)
+        e_swapped = setup%full_energy(config)
+
+        delta_e = e_swapped - e_unswapped
+
+        ! Accept or reject move
+        if (e_swapped > target_energy .and. delta_e < 0) then
+          e_unswapped = e_swapped
+        else if (e_swapped < target_energy .and. delta_e > 0) then
+          e_unswapped = e_swapped
+        else if (genrand() .lt. 0.001_real64) then ! to prevent getting stuck in local minimum (should adjust this later to something more scientific instead of an arbitrary number)
+          e_unswapped = e_swapped
+        else
+          call pair_swap(config, rdm1, rdm2)
+        end if
+      end if
+    end do
+
+  end subroutine burn_in
